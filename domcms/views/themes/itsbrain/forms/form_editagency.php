d437428884aa25b3311136101eb8c4999d4f2a07
Jeremy Davis <jwdavis@dealeronlinemarketing.com>
2013-01-21 07:59:39 -0500

    Error messages and add and edit agency pages

---
+++ domcms/views/themes/itsbrain/forms/form_editagency.php
@@ -0,0 +1,29 @@
+<!-- Content -->
+<div class="content" id="container">
+    <div class="title"><h5>Admin</h5></div>
+    <!-- Form begins -->
+    <form id="usualValidate" class="mainForm" method="post" action="">
+        <!-- Input text fields -->
+        <fieldset>
+            <div class="widget first">
+                <div class="head"><h5 class="iList">Edit Agency</h5></div>
+                <div class="rowElem noborder">
+                    <label>Agency Name<span class="req">*</span></label>
+                    <div class="formRight"><input type="text" class="required" name="name" id="name"/></div><div class="fix"></div>
+                </div>
+                <div class="rowElem noborder">
+                    <label>Agency Description</label>
+                    <div class="formRight">
+                        <textarea name="notes" rows="8" cols="" class="auto" name="notes"></textarea>
+                    </div>
+                    <div class="fix"></div>
+                </div>
+                <div class="submitForm">
+                    <input type="submit" value="submit" class="redBtn" />
+                </div>
+                <div class="fix"></div>
+            </div>
+        </fieldset>
+    </form>        
+</div>
+<div class="fix"></div>
