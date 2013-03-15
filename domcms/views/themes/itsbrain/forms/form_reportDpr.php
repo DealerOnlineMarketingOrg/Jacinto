d70f1bbd366727c8728117dcafff0d80b3a1846d
pkazda <pkazda@dealeronlinemarketing.com>
2013-03-15 17:35:12 -0400

    dpr

--- domcms/views/themes/itsbrain/forms/form_reportDpr.php
+++ domcms/views/themes/itsbrain/forms/form_reportDpr.php
@@ -14,20 +14,20 @@
     ?>
 	        <!-- Input text fields -->
         <fieldset>
-            <div id="reportID" class="widget first">
+            <div class="widget first">
                 <div class="head"><h5 class="iList">DPR Report</h5>
                 	<div style="width:1;float:right;vertical-align:middle">
                         <input ID="excel" class="greyishBtn" type="button" value="Excel" />
                         <input ID="pdf" class="greyishBtn" type="button" value="PDF" />
-                        <!-- <input ID="test" class="greyishBtn" type="button" value="Test" /> -->
+                        <input ID="test" class="greyishBtn" type="button" value="Test" />
                     </div>
                 </div>
                 <div class="rowElem noborder">
                     <!-- Line graph of year -->
                     <div class="widgets">
                         <div class="left">
-                            <div id="lineID" class="widget"><!-- Pie chart 1 -->
-                                <div class="head"><h5 id="update" class="iGraph">Leads Per Month</h5></div>
+                            <div class="widget"><!-- Pie chart 1 -->
+                                <div class="head"><h5 id="update" class="iGraph">Line Graph</h5></div>
                                 <div class="body">
                                 	<? echo $report_lineChart; ?>
                                 </div>
@@ -35,8 +35,8 @@
                         </div>
                         
                         <div class="right">
-                            <div id="pieID" class="widget"><!-- Pie chart 2 -->
-                                <div class="head"><h5 class="iChart8">Total Leads</h5></div>
+                            <div class="widget"><!-- Pie chart 2 -->
+                                <div class="head"><h5 class="iChart8">Pie Graph</h5></div>
                                 <div class="body">
                                     <? echo $report_pieChart; ?>
                                 </div>
@@ -58,37 +58,64 @@
     <?php echo  form_close(); ?>
     <script type="text/javascript">		
 		$(window).load (function() {
+			getVisualLayout();
 			plotLineChart();
 			plotPieChart();
 			elementToDataURL();
 		});
 		function plotLineChart() {<? echo $report_lineChart_script; ?>}
 		function plotPieChart() {<? echo $report_pieChart_script; ?>}
-		
-		$('input#test').click(function() {
-			elementToDataURL('#reportID');
-		});
-		function elementToDataURL($element) {
-			$($element).html2canvas({
+		function elementToDataURL() {
+			$('#reportBlock_lineChart').html2canvas({
 				onrendered: function (canvas) {
 					//Set hidden field's value to image data (base-64 string)
-					dataURL = canvas.toDataURL("image/png");
-					saveDataURL(dataURL);
+					//window.open(canvas.toDataURL("image/png"));
+					//Submit the form manually
+					document.getElementById("reportDpr").submit();
 				}
 			});
+			
+			//dataURL = canvasRecord.toDataURL("image/png");
+			//dataURL = dataURL.replace("image/png", "image/octet-stream");
+     		//window.open(dataURL);
 		}
-		function saveDataURL(dataURL) {
-			var fileName = "domcms/cache/img.png";
-			var imgData = { data: dataURL, destPath: fileName }
-			$.ajax({
-				type: 'POST',
-				url: "<?= base_url(); ?>file/saveDataURL",
-				data: imgData,
-				success:function(data) {
-					alert(data);
-				}
+		
+		$('input#test').click(function() {
+			alert('begin');
+			$layout = getVisualLayout();
+			$str = "";
+			for (var i = 0; i <$layout.length; i++) {
+				$str += $layout[i]['id'] + ",";
+			}
+			alert($str);
+		});
+		// Returns the visual layout of the rendered web page, starting from the specified element.
+		function getVisualLayout() {
+			// The report root object should have an ID of reportBlock
+			$str = "";
+			// Layout will be an array of objects, each one having id, left and top.
+			$Layout = [];
+			$("#reportBlock, #reportBlock *").each(function(index, domElem) {
+				$id = $(this).attr('id');
+				$text = $(this).text();
+				if ($text.search("1500") > -1)
+					{ alert("found"); }
+				try {
+					$offset = $(this).offset();
+					$Layout.push({
+						"id": $id,
+						"left": Math.round($offset['left']),
+						"top": Math.round($offset['top'])
+					});
+				} catch(e) {}
 			});
+			return $Layout;
 		}
+		
+		$(window).resize(function() {
+			$offset = $('h5#update').offset();
+			//$('h5#update').html('Offset = ' + Math.round($offset['left']) + ',' + Math.round($offset['top']));
+		});
 		
 		$('input#excel').click(function() {
 			var form_vars = {
@@ -98,16 +125,11 @@
 			$.ajax({
 				type: "post",
 				url: "<?= base_url(); ?>converter", 
-				data: form_vars,
+				data: {type:'excel',html:"<?php echo str_replace('"','\\"',$providers); ?>"},
 				success:function(data) {
 					alert(data);
-					//e.preventDefault();  //stop the browser from following
-					//window.location.href = filePath;
-				},
-				error:function(XMLHttpRequest, textStatus, errorThrown) {
-					alert(errorThrown);	
 				}
-			});
+			})
 			/*.done(function(data) { alert("success: " + data); })
 			.fail(function(xhr, textStatus) { alert("error " + xhr.responseText); })
 			.always(function() { alert("finished"); });*/
@@ -121,7 +143,7 @@
 			$.post('/converter', form_vars, function(data) {alert(data);}, "text");
 		});
 		
-		function saveImg() {
+		function writeImg() {
 			var divobj = document.getElementById("lineChart");
 			//var oImg = Canvas2Image.saveAsPNG(divobj.childNodes[0], true);
 			var canvas = divobj.childNodes[0];
