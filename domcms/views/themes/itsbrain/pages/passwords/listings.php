05fbe4406f541c7c388e80ba1dcfbfc6bcc3d673
Jeremy Davis <jwdavis@dealeronlinemarketing.com>
2013-04-22 11:59:08 -0400

    Passwords

--- domcms/views/themes/itsbrain/pages/passwords/listings.php
+++ domcms/views/themes/itsbrain/pages/passwords/listings.php
@@ -1,21 +1,24 @@
-    <div class="content">
-    	<div class="title"><h5><?php echo  $page_id; ?></div>
-        <?php notifyError(); ?>
-        <?php include 'domcms/views/themes/global/breadcrumb.php'; ?>
-    	<div class="table" style="margin-top:1px;">
-            <?php echo  $page_html; ?>
-            </div>
-         </div>
-        <div class="fix"></div>
+<div class="content">
+    <div id="loader_block">
+        <div id="client_loader"><img src="<?= base_url() . THEMEIMGS; ?>loaders/loader2.gif" /></div>
+    </div>
+    <div class="title"><h5>Passwords</div>
+    <?php notifyError(); ?>
+    <?php include 'domcms/views/themes/global/breadcrumb.php'; ?>
+    <div class="table" style="margin-top:5px;">
+        <div class="head"><h5 class="iView">View All Passwords</h5></div>
+        	<div id="passwordListTable"><?= PasswordlistTable(); ?></div>
+        </div>
     </div>
     <div class="fix"></div>
-    <style type="text/css">
-		div.dataTables_filter{top:-72px;}
-	</style>
-	<?php if (isset($page_scripts)) {
-		echo '<script type="text/javascript">';
-		// These are general page scripts for the current view.
-		echo $page_scripts;
-		echo '</script>';
-		}
-	?>
+</div>
+<div class="fix"></div>
+<div id="addPasswordsInfo"></div>
+<div id="editPasswordsInfo"></div>
+<div id="viewPasswordsInfo"></div>
+<style type="text/css">
+    div#loader_block{position:fixed;width:100%;height:100%;top:0;left:0;background:#fff;opacity:0.8;z-index:2000;display:none;}
+    div#client_loader{position:absolute;width:16px;height:16px;top:50%;margin-top:-8px;left:50%;margin-left:-8px;}
+    div#dataClient{margin-top:1px;}
+</style>
+<script type="text/javascript" src="<?= base_url(); ?>assets/themes/itsbrain/js/passwords_popups.js"></script>
